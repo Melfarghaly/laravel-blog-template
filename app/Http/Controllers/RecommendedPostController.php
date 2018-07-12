9c9758f26a65c814e81db20c881b5d5902783fac
weybansky <weybansky@gmail.com>
2018-07-12 14:19:15 +0100

    Started working on Post Recommendation for Editors

---
+++ app/Http/Controllers/RecommendedPostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RecommendedPost;
+use Illuminate\Http\Request;
+
+class RecommendedPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RecommendedPost  $recommendedPost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RecommendedPost $recommendedPost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RecommendedPost  $recommendedPost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RecommendedPost $recommendedPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RecommendedPost  $recommendedPost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RecommendedPost $recommendedPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RecommendedPost  $recommendedPost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RecommendedPost $recommendedPost)
+    {
+        //
+    }
+}
